f98d7d3957b90b96a473f1d4683779f77b43551a
wtt <1136220284@qq.com>
2024-02-01 15:40:32 +0800

    refactor: update

--- ozhera-log/log-stream/src/main/java/com/xiaomi/mone/log/stream/job/extension/MessageSenderFactory.java
+++ ozhera-log/log-stream/src/main/java/com/xiaomi/mone/log/stream/job/extension/MessageSenderFactory.java
@@ -1,8 +1,24 @@
+/*
+ * Copyright 2020 Xiaomi
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
 package com.xiaomi.mone.log.stream.job.extension;
 
 import com.xiaomi.mone.es.EsProcessor;
 import com.xiaomi.mone.log.api.enums.LogStorageTypeEnum;
 import com.xiaomi.mone.log.stream.job.SinkJobConfig;
+import com.xiaomi.mone.log.stream.job.extension.impl.DorisMessageSender;
 import com.xiaomi.mone.log.stream.job.extension.impl.EsMessageSender;
 import com.xiaomi.mone.log.stream.job.extension.impl.RocketMqMessageProduct;
 import com.xiaomi.mone.log.stream.plugin.es.EsPlugin;
@@ -26,7 +42,7 @@ public class MessageSenderFactory {
             case ELASTICSEARCH:
                 return getEsMessageSender(sinkJobConfig, mqMessageProduct);
             case DORIS:
-//                return getDorisMessageSender(sinkJobConfig, mqMessageProduct);
+                return getDorisMessageSender(sinkJobConfig, mqMessageProduct);
             default:
                 return null;
         }
@@ -40,8 +56,7 @@ public class MessageSenderFactory {
         return esMessageSender;
     }
 
-//    private static MessageSender getDorisMessageSender(SinkJobConfig sinkJobConfig, MqMessageProduct mqMessageProduct) {
-//        List<String> keyListSlice = IndexUtils.getKeyListSlice(sinkJobConfig.getKeyList());
-//        return new DorisMessageSender(sinkJobConfig.getIndex(), mqMessageProduct, sinkJobConfig.getStorageInfo(), keyListSlice);
-//    }
+    private static MessageSender getDorisMessageSender(SinkJobConfig sinkJobConfig, MqMessageProduct mqMessageProduct) {
+        return new DorisMessageSender(sinkJobConfig.getIndex(), mqMessageProduct, sinkJobConfig.getStorageInfo(), sinkJobConfig.getColumnList());
+    }
 }
